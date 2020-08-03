3f8327a2a143821b4649c4edd8943f73aedf9a60
Gaetan FOURNIER <gaefourn@e1r2p20.42.fr>
2020-08-03 21:39:43 +0200

    auto save

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+minikube delete
+
 minikube config set vm-driver virtualbox
 
 minikube start --cpus=2 --memory 4000
@@ -8,9 +10,11 @@ minikube addons enable metallb
 
 eval $(minikube docker-env)
 #DOCKERFILES HERE
-#docker build -t nginx srcs/nginx
+docker build -t nginx srcs/nginx
 #docker build -t mysql srcs/MySQL
-docker build -t phpmyadmin srcs/PhpMyAdmin
+#docker build -t phpmyadmin srcs/PhpMyAdmin
 #docker build -t wordpress srcs/wordpress
 
+kubectl create configmap confnginx --from-file=./srcs/nginx/nginx.conf
 kubectl describe cm config -n metallb-system
+kubectl apply -k srcs
