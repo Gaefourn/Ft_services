05fb9a8257b97394240a89823eef09116d1cba79
Gaetan FOURNIER <gaefourn@e1r4p6.42.fr>
2020-08-05 22:19:08 +0200

    auto save

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ minikube addons enable dashboard
 minikube addons enable metallb
 
 eval $(minikube docker-env)
-#DOCKERFILES HERE
 docker build -t nginx srcs/nginx
 docker build -t mysql srcs/MySQL
 docker build -t phpmyadmin srcs/PhpMyAdmin
